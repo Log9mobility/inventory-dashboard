8a45b4b0fa40364a71979b72b4865d302d363ce1
Log9mobility <134068748+Log9mobility@users.noreply.github.com>
2024-04-29 14:09:17 +0530

    inventory_dashboard.py

--- inventory_dashboard.py
+++ inventory_dashboard.py
@@ -42,15 +42,15 @@ def main():
         plt.rcParams['font.size'] = 12  # Adjust font size of labels
         st.pyplot(fig_ops_status)
 
-    # Fetch data from 'odoo_inventory' table for 'partner_id'
+    # Fetch top 10 partner IDs from 'odoo_inventory' table
     data_partner_id = fetch_data_from_supabase('partner_id')
 
     if data_partner_id is not None:
         # Count occurrences of each partner_id
-        partner_id_counts = pd.Series(data_partner_id).value_counts()
+        partner_id_counts = pd.Series(data_partner_id).value_counts().head(10)
 
         # Position the pie chart for 'partner_id' in the second half, right side of the page
-        st.write("## Partner ID Pie Chart")
+        st.write("## Top 10 Partner ID Pie Chart")
         fig_partner_id, ax_partner_id = plt.subplots(figsize=(8, 6))
         ax_partner_id.pie(partner_id_counts, labels=partner_id_counts.index, autopct='%1.1f%%', startangle=90)
         ax_partner_id.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
