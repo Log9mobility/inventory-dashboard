39e90a1fa9bea5dd02a0108a160986d239a5289d
Log9mobility <134068748+Log9mobility@users.noreply.github.com>
2024-05-03 13:38:32 +0530

    inventory_dashboard.py

--- inventory_dashboard.py
+++ inventory_dashboard.py
@@ -138,11 +138,12 @@ def main():
             # Create DataFrame
             df_partner_id_count = pd.DataFrame(data_partner_id_count, columns=['deployed_city', 'partner_id'])
             # Group by deployed_city and count partner_id
-            partner_id_count_table = df_partner_id_count.groupby('partner_id')['partner_id'].value_counts().unstack(fill_value=0)
+            partner_id_count_table = df_partner_id_count.groupby('deployed_city')['partner_id'].value_counts().unstack(fill_value=0)
             # Display partner_id count table
             st.write("## Table: Count of Partner IDs Across Deployed Cities")
             st.write(partner_id_count_table)
 
 if __name__ == "__main__":
     main()
+
 
