4496ac1b1c3126b7e510bebe17404c1ea979ac61
Log9mobility <134068748+Log9mobility@users.noreply.github.com>
2024-06-04 17:43:17 +0530

    inventory_dashboard.py

--- inventory_dashboard.py
+++ inventory_dashboard.py
@@ -3,7 +3,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-# Update the fetch_data_from_supabase function to dynamically determine the index of the 'region' column
+# Update the fetch_data_from_supabase function to not consider 'region' column
 def fetch_data_from_supabase(columns, filters):
     try:
         with psycopg2.connect(
@@ -23,22 +23,12 @@ def fetch_data_from_supabase(columns, filters):
                             values = tuple(map(str, values))
                             query += f" AND {column} IN {values}"
                 cursor.execute(query)
-                
-                # Fetch column names
                 data_columns = [desc[0] for desc in cursor.description]
-                
                 data = cursor.fetchall()
-                
-                # Filter records based on 'region' in Python code
-                region_values = filters.get('region', [])
-                if region_values:
-                    region_index = data_columns.index('region')
-                    data = [row for row in data if row[region_index] in region_values]
-                
-                return data
+                return data_columns, data
     except psycopg2.Error as e:
         st.error(f"Error connecting to Supabase: {e}")
-        return None
+        return None, None
 
 # Function to fetch distinct values for a column from Supabase table
 def fetch_distinct_values(column_name):
@@ -86,13 +76,6 @@ def main():
     region_options = ['West', 'North', 'South', 'Not Known']
     selected_regions = st.sidebar.multiselect('Select Region', region_options)
     
-    # Map deployed cities to regions and filter if both filters are used
-    regions = []
-    if deployed_city:
-        regions = [get_region(city) for city in deployed_city]
-    if selected_regions:
-        regions = [region for region in regions if region in selected_regions]
-    
     distinct_ops_status = fetch_distinct_values('ops_status')
     ops_status = st.sidebar.multiselect('Select Ops Status', distinct_ops_status)
 
@@ -105,10 +88,9 @@ def main():
     distinct_registration_numbers = fetch_distinct_values('registration_number')
     registration_number = st.sidebar.multiselect('Select Registration Number', distinct_registration_numbers)
 
-    # Create a dictionary to store all filters
+    # Create a dictionary to store all filters except 'region'
     filters = {
         'battery_capacity': battery_capacity,
-        'region': regions,
         'deployed_city': deployed_city,
         'ops_status': ops_status,
         'partner_id': partner_id,
@@ -117,11 +99,15 @@ def main():
     }
 
     # Fetch data from 'odoo_inventory' table for 'ops_status' with optional filters
-    data_ops_status = fetch_data_from_supabase(['ops_status'], filters)
+    data_columns, data_ops_status = fetch_data_from_supabase(['deployed_city', 'ops_status'], filters)
 
     if data_ops_status is not None:
+        # Filter based on selected regions
+        if selected_regions:
+            data_ops_status = [row for row in data_ops_status if get_region(row[data_columns.index('deployed_city')]) in selected_regions]
+
         # Calculate counts for 'rev gen' and 'non rev gen'
-        ops_status_list = [item[0] for item in data_ops_status]
+        ops_status_list = [row[data_columns.index('ops_status')] for row in data_ops_status]
         rev_gen_count = sum(1 for status in ops_status_list if status in ['RENTAL', 'PORTER'])
         non_rev_gen_count = len(ops_status_list) - rev_gen_count
         total_count = len(ops_status_list)
@@ -153,11 +139,15 @@ def main():
         st.plotly_chart(fig_ops_status)
 
         # Fetch data from 'odoo_inventory' table for 'partner_id'
-        data_partner_id = fetch_data_from_supabase(['partner_id'], filters)
+        data_columns, data_partner_id = fetch_data_from_supabase(['deployed_city', 'partner_id'], filters)
 
         if data_partner_id is not None:
+            # Filter based on selected regions
+            if selected_regions:
+                data_partner_id = [row for row in data_partner_id if get_region(row[data_columns.index('deployed_city')]) in selected_regions]
+
             # Count occurrences of each partner_id and select top 10
-            partner_id_list = [item[0] for item in data_partner_id]
+            partner_id_list = [row[data_columns.index('partner_id')] for row in data_partner_id]
             partner_id_counts = pd.Series(partner_id_list).value_counts().head(10)
 
             # Display the pie chart for 'partner_id'
@@ -168,9 +158,13 @@ def main():
             st.plotly_chart(fig_partner_id)
 
         # Fetch data for pivot table
-        data_pivot = fetch_data_from_supabase(['deployed_city', 'ops_status'], filters)
+        data_columns, data_pivot = fetch_data_from_supabase(['deployed_city', 'ops_status'], filters)
 
         if data_pivot is not None:
+            # Filter based on selected regions
+            if selected_regions:
+                data_pivot = [row for row in data_pivot if get_region(row[data_columns.index('deployed_city')]) in selected_regions]
+
             # Create DataFrame
             df_pivot = pd.DataFrame(data_pivot, columns=['deployed_city', 'ops_status'])
             # Pivot table
@@ -180,9 +174,13 @@ def main():
             st.write(pivot_table)
 
         # Fetch data for partner_id count table
-        data_partner_id_count = fetch_data_from_supabase(['deployed_city', 'partner_id'], filters)
+        data_columns, data_partner_id_count = fetch_data_from_supabase(['deployed_city', 'partner_id'], filters)
 
         if data_partner_id_count is not None:
+            # Filter based on selected regions
+            if selected_regions:
+                data_partner_id_count = [row for row in data_partner_id_count if get_region(row[data_columns.index('deployed_city')]) in selected_regions]
+
             # Create DataFrame
             df_partner_id_count = pd.DataFrame(data_partner_id_count, columns=['deployed_city', 'partner_id'])
             # Group by partner_id and count deployed_city
@@ -192,9 +190,13 @@ def main():
             st.write(partner_id_count_table)
 
         # Fetch data for deployed assets table
-        data_deployed_assets = fetch_data_from_supabase(['deployed_city', 'chassis_number', 'partner_id', 'battery_capacity', 'ops_status'], filters)
+        data_columns, data_deployed_assets = fetch_data_from_supabase(['deployed_city', 'chassis_number', 'partner_id', 'battery_capacity', 'ops_status'], filters)
 
         if data_deployed_assets is not None:
+            # Filter based on selected regions
+            if selected_regions:
+                data_deployed_assets = [row for row in data_deployed_assets if get_region(row[data_columns.index('deployed_city')]) in selected_regions]
+
             # Create DataFrame
             df_deployed_assets = pd.DataFrame(data_deployed_assets, columns=['deployed_city', 'chassis_number', 'partner_id', 'battery_capacity', 'ops_status'])
             
