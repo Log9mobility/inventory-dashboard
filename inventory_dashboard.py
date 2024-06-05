f52865a1439dfead2deb877484c1a86f6b1757c8
Log9mobility <134068748+Log9mobility@users.noreply.github.com>
2024-06-05 18:42:46 +0530

    inventory_dashboard.py

--- inventory_dashboard.py
+++ inventory_dashboard.py
@@ -165,7 +165,7 @@ def main():
         total_count = len(ops_status_list)
 
         # Calculate %Utilization using existing counts
-        #utilization_percentage = (rev_gen_count / total_count) * 100 if total_count != 0 else 0
+        utilization_percentage = (rev_gen_count / total_count) * 100 if total_count != 0 else 0
 
         # Create DataFrame for rev_gen, non_rev_gen, and total counts
         df_counts = pd.DataFrame({
@@ -176,18 +176,19 @@ def main():
         # Display the %Utilization and revenue generation count in two columns at the top of the page
         col1, col2 = st.columns([1, 1])
         with col1:
-            #st.write("## %Utilization")
-            #st.write(f"{utilization_percentage:.2f}%")
-        # Display the %Utilization for each region
-            st.write("## %Utilization by Region")
-            region_utilization_df = pd.DataFrame(list(region_utilization.items()), columns=['Region', '%Utilization'])
-            st.write(region_utilization_df)
+            st.write("## %Utilization")
+            st.write(f"{utilization_percentage:.2f}%")
         with col2:
             st.write("## Revenue Generation and Non-Revenue Generation Counts")
             st.write(df_counts)
 
         # Calculate %Utilization for each region
         region_utilization = calculate_region_utilization(data_ops_status, selected_regions)
+
+        # Display the %Utilization for each region
+        st.write("## %Utilization by Region")
+        region_utilization_df = pd.DataFrame(list(region_utilization.items()), columns=['Region', '%Utilization'])
+        st.write(region_utilization_df)
 
         # Display the pie chart for ops_status
         st.write("## Ops Status Pie Chart")
@@ -268,7 +269,7 @@ def main():
             if selected_regions:
                 data_deployed_assets = [row for row in data_deployed_assets if get_region(row[data_columns.index('deployed_city')]) in selected_regions]
 
-                        # Create DataFrame
+                       # Create DataFrame
             df_deployed_assets = pd.DataFrame(data_deployed_assets, columns=['deployed_city', 'chassis_number', 'partner_id', 'battery_capacity', 'ops_status'])
             
             # Display the table
